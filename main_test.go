f0318f3847e1c879d7be5c5996e9f33f670784fe
Scott Ranger <scottranger@protonmail.ch>
2018-05-18 21:16:56 +0000

    basic create key example

--- main_test.go
+++ main_test.go
@@ -4,14 +4,37 @@ import "testing"
 import "github.com/coreos/etcd/clientv3"
 import "os"
 import "time"
+import "context"
 
 func TestCanConnectToETCD(t *testing.T) {
-	_, err := clientv3.New(clientv3.Config{
+	client, err := clientv3.New(clientv3.Config{
 		Endpoints:   []string{os.Getenv("ETCD")},
 		DialTimeout: 2 * time.Second,
 	})
 
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
+
+	defer client.Close()
+}
+
+func TestCanCreateKey(t *testing.T) {
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{os.Getenv("ETCD")},
+		DialTimeout: 2 * time.Second,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer client.Close()
+
+	_, err = client.Put(context.Background(), "key", "value")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 }
