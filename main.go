fcefbe142ce3579b5d2b9233d791a6f70e504d09
Scott Ranger <scottranger@protonmail.ch>
2018-05-18 20:50:57 +0000

    test to prove etcd build interaction works

--- main.go
+++ main.go
@@ -5,7 +5,3 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world")
 }
-
-func add(x,y int) int {
-	return x + y
-}
--- main_test.go
+++ main_test.go
@@ -1,11 +1,17 @@
 package main
 
 import "testing"
+import "github.com/coreos/etcd/clientv3"
+import "os"
+import "time"
 
-func TestAdd(t *testing.T) {
-	result := add(1, 2)
+func TestCanConnectToETCD(t *testing.T) {
+	_, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{os.Getenv("ETCD")},
+		DialTimeout: 2 * time.Second,
+	})
 
-	if result != 3 {
-		t.Error("Fail")
+	if err != nil {
+		t.Error(err)
 	}
 }
